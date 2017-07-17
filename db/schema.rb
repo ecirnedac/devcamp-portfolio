7e2dc6c0a9dff28a4ff8c5cb633b3da650b16645
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 17:46:20 +0000

    removed merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170713164107) do
-=======
-ActiveRecord::Schema.define(version: 20170712165059) do
->>>>>>> data-feature
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +72,6 @@ ActiveRecord::Schema.define(version: 20170712165059) do
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -93,9 +89,3 @@ ActiveRecord::Schema.define(version: 20170712165059) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
-
-=======
->>>>>>> data-feature
-  add_foreign_key "blogs", "topics"
-  add_foreign_key "technologies", "portfolios"
-end
