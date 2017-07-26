957b46b89a3287ead6c57fa42f87ddd65f065d17
Jordan Hudgens <jordan@devcamp.com>
2017-07-26 16:22:52 +0000

    Implemented partials with local data fro portfolio for and nav bars

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,20 +1,19 @@
 class PortfoliosController < ApplicationController
   layout 'portfolio'
   
-  
   def index
     @portfolio_items = Portfolio.all
   end
-  
+
   def angular
     @angular_portfolio_items = Portfolio.angular
   end
-  
+
   def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
   end
-  
+
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
 
@@ -26,13 +25,15 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
+
   def edit
-    @portfolio_item = Portfolio.find (params[:id])
+    @portfolio_item = Portfolio.find(params[:id])
+
   end
-  
+
   def update
     @portfolio_item = Portfolio.find(params[:id])
+
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -41,32 +42,32 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
+
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
-  
+
   def destroy
     # Perform the lookup
     @portfolio_item = Portfolio.find(params[:id])
-    
-    # Destroy the record
+
+    # Destroy/delete the record
     @portfolio_item.destroy
-    
+
     # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
-  
+
   private
-  
+
   def portfolio_params
     params.require(:portfolio).permit(:title,
-                                      :subtitle, 
-                                      :body, 
+                                      :subtitle,
+                                      :body,
                                       technologies_attributes: [:name]
-                                      )
+                                     )
   end
-  
+
 end
